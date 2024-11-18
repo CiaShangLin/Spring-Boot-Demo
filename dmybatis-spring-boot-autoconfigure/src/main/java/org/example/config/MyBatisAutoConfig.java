5a98ff8def2ef393b21362a1cf504f2400f48bdd
CaiShangLin <nick034po@yahoo.com.tw>
2024-11-19 00:30:06 +0800

    2024/11/19 1.學習自訂義starter

---
+++ dmybatis-spring-boot-autoconfigure/src/main/java/org/example/config/MyBatisAutoConfig.java
@@ -0,0 +1,36 @@
+package org.example.config;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@AutoConfiguration
+public class MyBatisAutoConfig {
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer(BeanFactory beanFactory) {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+
+        List<String> packages = AutoConfigurationPackages.get(beanFactory);
+        String p = packages.get(0);
+        mapperScannerConfigurer.setBasePackage(p);
+
+        mapperScannerConfigurer.setAnnotationClass(Mapper.class);
+        return mapperScannerConfigurer;
+    }
+}
