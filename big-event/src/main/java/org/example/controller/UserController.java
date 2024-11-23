1f7705f385cf0dd481b37c46602cf50ebb47f3ab
CaiShangLin <nick034po@yahoo.com.tw>
2024-11-23 22:31:56 +0800

    2024/11/23 1.實戰篇-05

--- big-event/src/main/java/org/example/controller/UserController.java
+++ big-event/src/main/java/org/example/controller/UserController.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Pattern;
 import org.example.pojo.Result;
 import org.example.pojo.User;
 import org.example.service.UserService;
+import org.example.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +18,25 @@ public class UserController {
     UserService userService;
 
     @PostMapping("register")
-    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}$") String password){
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         User user = userService.findByUserName(username);
-        if(user==null){
-            userService.register(username,password);
+        if (user == null) {
+            userService.register(username, password);
             return Result.success();
-        }else{
+        } else {
             return Result.error("用戶名已被占用");
         }
+    }
+
+    @PostMapping("login")
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+        User user = userService.findByUserName(username);
+        if(user == null){
+            return Result.error("沒有用戶名");
+        }
+        if(MD5Utils.encrypt(password).equals(user.getPassword())){
+            return Result.success("jwt token...");
+        }
+        return Result.error("密碼錯誤");
     }
 }
